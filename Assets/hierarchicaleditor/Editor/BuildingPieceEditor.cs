226e5eb568d63fe10d7152ca3f0f13124863f9da
Juanita-Benjamin <juanitabenjamin98@gmail.com>
2026-03-02 11:46:56 -0500

    Still the same

--- Assets/hierarchicaleditor/Editor/BuildingPieceEditor.cs
+++ Assets/hierarchicaleditor/Editor/BuildingPieceEditor.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
-using UnityEditor.Experimental.TerrainAPI;
+//using UnityEditor.Experimental.TerrainAPI;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
