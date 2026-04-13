d77f5b7928cd222ff318baad286d161a60bd1522
Juanita-Benjamin <juanitabenjamin98@gmail.com>
2026-04-13 17:45:25 -0400

    this the working version

---
+++ Assets/Scripts/Motion.cs
@@ -0,0 +1,112 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using XRTLogging;
+
+public class Motion : MonoBehaviour
+{
+    public List<GameObject> MotionObjects;
+    private string ParticipantID;
+    private string Personality;
+
+
+    public TMP_Dropdown PID;
+
+    private StreamWriter writer;
+    private StringBuilder stringBuilder;
+    private string log_path;
+    public bool startTracking;
+    private float writingTime = 60.0f;
+    private float timeElapsed = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        stringBuilder = new StringBuilder();
+    }
+
+    public void StartTrackingOn()
+    {
+        startTracking = true;
+
+
+        if (startTracking)
+        {
+            //handles the creation of the motion tracking for each object
+            var currentScene = SceneManager.GetActiveScene();
+            ParticipantID = PID.options[PID.value].text;
+           
+            //I changed from Tracking to New_Tracking - 11.26.25
+            log_path = $"Tracking/{ParticipantID}_{DateTime.Now.ToString("MMddyy-HHmm")}.csv";
+            if (!Directory.Exists($"New_Tracking/{currentScene.name}"))
+            {
+                Directory.CreateDirectory($"New_Tracking/{currentScene.name}");
+            }
+            //string headers = "Timestamp, TimeElapsed,"; //placeholder for now
+            string headers = "TimeStamp,";
+            for (int i = 0; i < MotionObjects.Count; i++)
+            {
+                string objName = MotionObjects[i].name;
+                string label = "Label";
+                headers += $"{objName}_position_x,{objName}_position_y,{objName}_position_z, {objName}_quat_x,{objName}_quat_y,{objName}_quat_z,{objName}_quat_w,{objName}_six_a,{objName}_six_b,{objName}_six_c,{objName}_six_d, {objName}_Six_e, {objName}_Six_f, {label}";
+
+            }
+
+            //this will add the headers to the .csv file
+            writer = new StreamWriter(log_path);
+            writer.WriteLine(headers);
+
+        }
+
+        Debug.Log("tracking started");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (startTracking)
+        {
+            //Add Label definition here
+           
+            //var  = PID.GetComponent<TMP_InputField>().text;
+          
+
+            timeElapsed += Time.deltaTime;
+            //stringBuilder.Append($"\n{DateTime.Now.ToUniversalTime()}, {timeElapsed:0.0000},");
+            stringBuilder.Append($"\n{timeElapsed:0.0000},"); //Using this for ML format 11.26.25
+            for (int i = 0; i < MotionObjects.Count; i++)
+            {
+                Quaternion quaternion = MotionObjects[i].transform.rotation;
+                float[] sixDegrees = SixDConversions.To6D(quaternion); //convert the Quaternion values to 6DoF
+                stringBuilder.Append($"{MotionObjects[i].transform.position.x:0.0000},{MotionObjects[i].transform.position.y:0.0000},{MotionObjects[i].transform.position.z:0.0000},{MotionObjects[i].transform.rotation.x:0.0000}, {MotionObjects[i].transform.rotation.y:0.0000}, {MotionObjects[i].transform.rotation.z:0.0000},{MotionObjects[i].transform.rotation.w:0.0000},");
+                stringBuilder.Append($"{sixDegrees[0]:0.0000}, {sixDegrees[1]:0.0000},{sixDegrees[2]:0.0000},{sixDegrees[3]:0.0000}, {sixDegrees[4]:0.0000}, {sixDegrees[5]:0.0000},");
+                //Debug.Log(MotionObjects[i].transform.position.x);
+
+                //stringBuilder.Append($"{personality} | {currentScene.name.Substring(0,1)} | {gender.Substring(10, 1)}");
+            }
+
+        }
+        //writing to the disk 
+        writingTime -= Time.deltaTime;
+        if (writingTime <= 0.0f)
+        {
+            writer.Write(stringBuilder.ToString());
+            stringBuilder.Clear();
+            writingTime = 60.0f;
+        }
+    }
+
+    public void StopTrackingOff()
+    {
+        //turn the logging off
+        startTracking = false;
+        Debug.Log("tracking stopped");
+        writer.Close();
+    }
+}
--- Assets/Scripts/ParticipantLog.cs
+++ Assets/Scripts/ParticipantLog.cs
@@ -6,6 +6,7 @@ using System.IO;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using TMPro.Examples;
 
 
 
@@ -13,7 +14,7 @@ public class ParticipantLog : MonoBehaviour
 {
     
     public TMP_Dropdown cohort_dropdown, gender_dropdown, PID_dropdown;
-    
+    public TextMeshProUGUI time;
     
     //don't use readonly key word
     string log_path = "Participant_Logs.csv";
@@ -23,23 +24,23 @@ public class ParticipantLog : MonoBehaviour
     public void ParticipantData()
     {
         string dateTime = DateTime.Now.ToString("MM-dd-yy-HH-mm");
-        string ParticipantID = PID_dropdown.options[gender_dropdown.value].text;
+        string ParticipantID = PID_dropdown.options[PID_dropdown.value].text;
         string gender = gender_dropdown.options[gender_dropdown.value].text;
         string cohort = cohort_dropdown.options[cohort_dropdown.value].text;
 
         if (!File.Exists(log_path))
         {
             using StreamWriter writer = File.CreateText(log_path);
-            writer.WriteLine("ParticipantID, Gender, date-time");
+            writer.WriteLine("ParticipantID, Gender, Cohort, date-time, Time");
 
-            writer.WriteLine($"{ParticipantID}, {gender}, {cohort}, {dateTime}");
+            writer.WriteLine($"{ParticipantID}, {gender}, {cohort}, {dateTime}, {time.text}");
             writer.Close();
         }
 
         else 
         { 
             using StreamWriter writer = File.AppendText(log_path);
-            writer.WriteLine($"{ParticipantID}, {gender}, {cohort}, {dateTime}");
+            writer.WriteLine($"{ParticipantID}, {gender}, {cohort}, {dateTime}, {time.text}");
             writer.Close();
         }
 
